7f72906c1c89983791e9c957038c17eb70eaf034
eliot owiti <ellyman2021@gmail.com>
2026-03-28 17:14:20 +0300

    Fix YouTube video embed and update sermon details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2229,7 +2229,7 @@ const Home = () => {
                 <iframe 
                   width="100%" 
                   height="315" 
-                  src="https://www.youtube.com/watch?v=6uic_6nIxvs" 
+                  src="https://www.youtube.com/embed/6uic_6nIxvs?start=137" 
                   title="Featured Sermon" 
                   frameBorder="0" 
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
@@ -2258,7 +2258,7 @@ const Home = () => {
                 Diocesan Easter Carols
               </p>
               <div className="sermon-actions">
-                <a href="https://www.youtube.com/watch?v=6uic_6nIxvs" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                <a href="https://www.youtube.com/watch?v=6uic_6nIxvs&t=137s" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
                   Watch on YouTube
                 </a>
                 <a href="#" className="btn btn-outline">
